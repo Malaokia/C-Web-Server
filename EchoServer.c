629d7ff8dbabdb32f8f7f0c9f302a28e3e93adaf
Tzinov <tzinov@ws-MacBook-Pro.local>
2015-09-13 23:58:58 -0600

    a lot of stuff

--- EchoServer.c
+++ EchoServer.c
@@ -11,10 +11,34 @@
 #define MAX_CLIENTS 32
 #define MAX_DATA 1024
 
+/* TODO */
+/*
+ * >>> Setup Up Config File Reading/Extacting
+ * 1.) Open file in existing filepath/directory (ws.conf), read it line by line
+ * 2.) After reading lines from file, extract fields that we need (regex? string search?)
+ * 3.) Populate our TextfileData struct with the fields that we extact from the conf file
+ * 4.) Using the data in our struct, set the variables for the server accordingly
+ *
+ * >>> Find out how to serve file content to client
+ *1.) Read/learn how the response body/header needs to be structured for different kinds of file types
+ *2.) Learn how to navigate directory structures (does file exist, get this file, etc) in C
+ *3.) Start with trying to serve up static html page
+ *3.) 
+
+ * >>> Find out / create function that will parse the request URL and:
+ *   - decide if it is a valid URL
+ *   - parse out the method type
+ *   - parse out the version of HTML
+ *   - parse out the file that is requested
+ *
+ * >>> Design logic for when to display 404 and when to display static index page based on URL
+
+*/
+void interpret_request(char *path);
+void removeSubstring(char *s,const char *toremove);
+void extract_request_path(char *response, char *file_path);
 int setup_socket (int port_number, int max_clients);
 void setup_server ();
-
-
 
 struct TextfileData {
   int port_number;
@@ -22,19 +46,11 @@ struct TextfileData {
   char *default_web_page;
 };
 
-
 int main(int argc, char ** argv)
 {
-
-  printf("inside of main\n");
-
-  int main_socket;
-  struct TextfileData system_config_data;
-
-  printf("about to call setup_server\n");
-  setup_server(&system_config_data); 
-  printf("About to call setup_socket!!!\n");
-  main_socket = setup_socket(system_config_data.port_number, MAX_CLIENTS);
+  printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+  printf("| Welcome to this wonderful C server! |\n");
+  printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
 
   char response[] = "HTTP/1.1 200 OK\r\n"
     "Content-Type: text/html; charset=UTF-8\r\n\r\n"
@@ -43,48 +59,89 @@ int main(int argc, char ** argv)
     "h1 { font-size:4cm; text-align: center; color: black;"
     " text-shadow: 0 0 2mm blue}</style></head>"
     "<body><h1>Hello World</h1></body></html>\r\n";
+  int main_socket;
+  struct TextfileData system_config_data;
+  struct sockaddr_in client;
+  int cli;
+  unsigned int sockaddr_len = sizeof(struct sockaddr_in);
+  int data_len;
+  char data[MAX_DATA];
+  char file_path[6];
 
-  //struct sockaddr_in client;
-  //int cli;
-  //unsigned int sockaddr_len = sizeof(struct sockaddr_in);
-  //int data_len;
-  //char data[MAX_DATA];
+  setup_server(&system_config_data); 
+  main_socket = setup_socket(system_config_data.port_number, MAX_CLIENTS);
 
 
-
-/*
   while (1)
   {
-
-    // we provide the accept command a pointer to an address structure that will
-    // contain information about the client
-    if ((cli = accept(main_socket, (struct sockaddr *)&client, &sockaddr_len)) == ERROR)
-    {
+    if ((cli = accept(main_socket, (struct sockaddr *)&client, &sockaddr_len)) == ERROR) {
       perror("accept");
       exit(-1);
     }
+    printf("New Client connected from port number %d and IP %s\n\n", ntohs(client.sin_port), inet_ntoa(client.sin_addr));
 
-    printf("New Client connected from port number %d and IP %s\n", ntohs(client.sin_port), inet_ntoa(client.sin_addr));
-
-    data_len = 1;
-
-    while (data_len)
-    {
-      data_len = recv(cli, data, MAX_DATA, 0);
-
-      if (data_len)
-      {
-        printf("Recieved Request: \n");
-        printf(" %s", data);
-        write(cli, response, sizeof(response) );
-        //printf("Client disconnected\n");
-        //close(cli);
-      }
+    data_len = recv(cli, data, MAX_DATA, 0);
+    if (data_len) {
+      extract_request_path((char *)&data, (char *)&file_path);
+      interpret_request((char *)&file_path);
+      write(cli, response, sizeof(response) -1);
+      printf("Client disconnected\n");
+      close(cli);
     }
   }
-  */
 }
 
+
+/*-------------------------------------------------------------------------------------------------------------------------------------------
+ * interpret_request - this function will be take the users path and decide what to do based on the result
+ *-------------------------------------------------------------------------------------------------------------------------------------------
+ */
+void interpret_request(char *path) {
+
+
+}
+
+
+
+/*-------------------------------------------------------------------------------------------------------------------------------------------
+ * extract_request_path - this function will be mainly responsible for parsing and extracting the path from the HTTP request from the client
+ *-------------------------------------------------------------------------------------------------------------------------------------------
+ */
+void extract_request_path(char *response, char *return_path) {
+
+
+  printf("Beginning of: extract_request_path()\n");
+  char *saveptr;
+  char *the_path;
+
+
+  // after this returns, return_path contains the first token while saveptr is a reference to the remaining tokens 
+  the_path = strtok_r(response, "\n", &saveptr);
+  removeSubstring(the_path, "HTTP/1.1");
+  printf( "First Token after seperating response by newline: \n%s\n", the_path);
+  //printf( "Remaining Tokens after seperating response by newline: \n%s\n", saveptr);
+
+  the_path = strtok_r(the_path, "/", &saveptr);
+  printf( "First token after seperation by slash character: \n%s\n", the_path );
+  //printf( "Remaining tokens after seperation by slash character: \n%s\n", saveptr );
+
+  the_path = strtok_r(NULL, " ", &saveptr);
+  printf( "Second token after seperation by slash character: %s\n", the_path );
+  //printf( "Remaining tokens after seperation by slash character: \n%s\n", saveptr );
+  printf("Leaving: extract_request_path()\n");
+  strcpy(return_path, the_path);
+
+}
+
+/*-------------------------------------------------------------------------------------------------------------------------------------------
+ * removeSubstring - this function is a helper function that is used when extracting the path that the client sends a GET request on
+ *-------------------------------------------------------------------------------------------------------------------------------------------
+ */
+void removeSubstring(char *s,const char *toremove)
+{
+  while( (s=strstr(s,toremove)) )
+    memmove(s,s+strlen(toremove),1+strlen(s+strlen(toremove)));
+}
 
 /*----------------------------------------------------------------------------------------------
  * setup_server - first function called on entry, prints information and reads in config file 
@@ -93,12 +150,12 @@ int main(int argc, char ** argv)
 
 void setup_server(struct TextfileData *config_data)
 {
+  printf("Beginning of: setup_server()\n");
   config_data->port_number = 10000;
   config_data->document_root = "doc root";
   config_data->default_web_page = "web page";
 
-  printf("Welcome to setup_server!! This is where we will pull data from the config file and populate our config struct\n");
-  printf("leaving setup_server\n");
+  printf("Leaving: setup_server()\n\n");
 }
 
 /*----------------------------------------------------------------------------------------------
@@ -107,7 +164,7 @@ void setup_server(struct TextfileData *config_data)
  */
 int setup_socket(int port_number, int max_clients)
 {
-  printf("At the beginning of setup_socket()");
+  printf("Beginning of: setup_socket()\n");
 
   /* The data structure used to hold the address/port information of the server-side socket */
   struct sockaddr_in server;
@@ -145,5 +202,6 @@ int setup_socket(int port_number, int max_clients)
     perror("Listen");
     exit(-1);
   }
-  return 0;
+  printf("Leaving: setup_socket()\n\n");
+  return sock;
 }
